e016a52668dce65740ca67483737e7ccdf9770b2
Nick Ramirez <nramirez@haproxy.com>
2019-01-14 13:02:02 -0500

    Updated init.sh to use newer install.sh script from JWT repo.

--- README.md
+++ README.md
@@ -7,15 +7,13 @@ Set up:
 1. Sign up for a free account at https://auth0.com/ and create a new API.
 1. Under the *Scopes* tab for the API, add a "read:hamsters" scope and a "write:hamsters" scope.
 1. Add a new "Machine to Machine Application" on the Auth0 site and select the API you created. Grant the "read" and "write" scopes.
-1. Replace the pem/pubkey.pem with the public key from your application's Settings tab: **Applications > My App > Settings > Show Advanced Settings > Certificates > Download Certificate**. 
+1. Replace the pem/pubkey.pem with the public key from your application's Settings tab: **Applications > My App > Settings > Show Advanced Settings > Certificates > Download Certificate**. (Be sure to convert it first using `openssl x509 -pubkey -noout -in ./mycert.pem > pubkey.pem`)
 1. Edit haproxy/haproxy.cfg, replacing the Auth0 URL with your own, such as https://myaccount.auth0.com/
 1. Create the environment with Vagrant:
-
-```
-vagrant up
-```
-
-1. Get a JSON web token (JWT) from your authentication server by following the *Quick Start* on the Auth0 website for your Machine to Machine application.
+    ```
+    vagrant up
+    ```
+1. Get a JSON web token (JWT) from your authentication server by following the *Quick Start* on the Auth0 website, under the Applications tab, for your Machine to Machine application.
 1. Make requests to https://192.168.50.20/api/hamsters and attach the JWT in the Authorization header. You should get a successful response.
 
 ## Troubleshooting
--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@
 # Install HAProxy-Lua-JWT library
 git clone https://github.com/haproxytech/haproxy-lua-jwt.git
 cd haproxy-lua-jwt
-chmod +x ./install-ubuntu.sh
-sudo ./install-ubuntu.sh
+chmod +x ./install.sh
+sudo ./install.sh luajwt
 
 # Install HAProxy
 sudo add-apt-repository ppa:vbernat/haproxy-1.9
@@ -15,20 +15,20 @@ sudo cp -rf /vagrant/haproxy/* /etc/haproxy/
 sudo systemctl restart haproxy
 
 # Install Docker
-if [ ! $(which docker) ]; then
-  sudo apt update
-  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  sudo apt update
-  sudo apt install -y docker-ce
-  sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-else
-  echo "docker already installed."
-fi
+# if [ ! $(which docker) ]; then
+#   sudo apt update
+#   sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+#   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#   sudo apt update
+#   sudo apt install -y docker-ce
+#   sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+#   sudo chmod +x /usr/local/bin/docker-compose
+# else
+#   echo "docker already installed."
+# fi
 
-# Start web app
-cd /vagrant/web
-docker-compose build
-docker-compose up -d
+# # Start web app
+# cd /vagrant/web
+# docker-compose build
+# docker-compose up -d
