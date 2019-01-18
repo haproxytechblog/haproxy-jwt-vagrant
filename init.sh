a821a05323d264d34e8e42bdcf1028a47a38452d
Nick Ramirez <nramirez@haproxy.com>
2019-01-18 10:27:17 -0500

    Uncommented Docker install

--- init.sh
+++ init.sh
@@ -16,19 +16,19 @@ sudo systemctl restart haproxy
 
 # Install Docker
 # if [ ! $(which docker) ]; then
-#   sudo apt update
-#   sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-#   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#   sudo apt update
-#   sudo apt install -y docker-ce
-#   sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-#   sudo chmod +x /usr/local/bin/docker-compose
-# else
-#   echo "docker already installed."
-# fi
+  sudo apt update
+  sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt update
+  sudo apt install -y docker-ce
+  sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+else
+  echo "docker already installed."
+fi
 
-# # Start web app
-# cd /vagrant/web
-# docker-compose build
-# docker-compose up -d
+# Start web app
+cd /vagrant/web
+docker-compose build
+docker-compose up -d
